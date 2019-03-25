dda3c7cda8c24cfb2d83537aaddde1f063cae059
Berdnikov Maxim <presslxqii@yandex.ru>
2019-03-25 21:14:20 +0700

    Init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import './css/init.css'
+import {Provider} from 'react-redux'
+import store from './redux/store'
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
